1628bef0e133c342947ffc211ee18f8c825b56da
Stereopapa <72004744+Stereopapa@users.noreply.github.com>
2026-02-26 01:15:08 +0100

    Refactor: structure reorganization, strict gitignore and repository cleanup

--- backend/database/db.py
+++ backend/database/db.py
@@ -5,6 +5,7 @@ from contextlib import contextmanager
 engine = create_engine("sqlite:///database/keylogger.db", echo=False)
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
 Base = declarative_base()
+Base.metadata.create_all(bind=engine)
 
 @contextmanager
 def get_db_session():
@@ -17,3 +18,4 @@ def get_db_session():
         raise
     finally:
         session.close()
+
